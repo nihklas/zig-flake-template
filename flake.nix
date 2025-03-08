62d55b9c89650c7c3781be534134da489e69e652
Niklas Koll <niklas@nkoll.de>
2025-03-08 12:53:24 +0100

    Fix path

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   outputs = {self}: {
     templates.default = self.templates."0.14.0";
     templates."0.14.0" = {
-      path = ./templates/default;
+      path = ./templates/0.14.0;
       description = "Zig Flake Template for 0.14.0";
     };
   };
